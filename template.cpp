038f2502a63e4b6a5aa63bd7ca129ccc1c580999
Diya Nag Chaudhury <44759773+cherryWood55@users.noreply.github.com>
2020-07-23 08:37:29 +0530

    Added template for kickstart

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define f(i, N) for(int i=0; i<N; i++)
+using namespace std;
+int main()
+{
+	int t, c=1;
+	cin >> t;
+	while (c<=t)
+	{
+		
+		cout << "Case #" << c << ": " ;
+		
+		cout << "\n";
+		c++;
+	}
+	return 0;
+}
