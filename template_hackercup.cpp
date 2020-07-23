495ba36862025a968e849614e811140ea24aaf5a
Diya Nag Chaudhury <44759773+cherryWood55@users.noreply.github.com>
2020-07-23 08:35:39 +0530

    Added template for hackercup

---
+++ template_hackercup.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define f(i,n) for (int i=0; i<n; i++)
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("_input1.txt", "r" , stdin);
+    freopen("_output1.txt", "w", stdout);
+    #endif
+    int c=1, t;
+    cin >> t;
+    while (c<=t)
+    {
+    	cout << "Case #" << c << ": ";
+    	
+    	c++;
+	}
+	return 0;
+}
