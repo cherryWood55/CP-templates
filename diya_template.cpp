9f1ee1c704ef92aa04cdbdd09b1b445bab72385a
Diya Nag Chaudhury <44759773+cherryWood55@users.noreply.github.com>
2020-08-29 09:18:51 +0530

    Update diya_template.cpp

--- diya_template.cpp
+++ diya_template.cpp
@@ -1,7 +1,10 @@
-//final template
 #include <bits/stdc++.h>
-#define ull_t unsigned long long int
-#define ll_t long long int 
+#include <cstdint>
+using namespace std;
+
+typedef unsigned long long int ull_t;
+typedef long long int ll_t;
+
 #define FAST_IO(val) std :: ios :: sync_with_stdio(val); std :: cin.tie(NULL); std :: cout.tie(NULL)
 #define debug(x) cout << #x << "=" << x << endl
 #define debug2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
@@ -23,49 +26,103 @@
 #define compute_parity(n) __builtin_parity(n)
 #define count_leading_zeroes(n) __builtin_clzll(n)
 #define count_trailing _zeroes(n) __builtin_ctzll(n)
-using namespace std;
+
 /*Commonly used functions
-long long int max_ll(long long int a, long long int b){
+ll_t maxLL(ll_t a, ll_t int b){
 	return (a>b?a:b);
 }
-long long int min_ll(long long int a, long long int b){
+ll_t minLL(ll_t a, ll_t b){
 	return (a<b?a:b);
 }
-long long int compute_total_divisors(long long int n){
-    long long int total_divisors = 0;
-    for(long long int i = 1; i <= (n / i); i++) 
-    {
-        if(!(n % i)) 
-        {
-            if((n / i) == i) 
-            {
+ll_t computeTotalDivisors(ll_t n){
+    ll_t total_divisors = 0;
+    for(ll_t i = 1; i <= (n / i); i++){
+        if(!(n % i)) {
+            if(n/i==i){
                 total_divisors++;
                 continue;
             }
             total_divisors += 2;
         }
     }
-    return total_divisors;
+    return (total_divisors);
 }
-bool check_perfect_square(long long int n){
-	return compute_total_divisors(n) % 2 ? true : false;
-}    
-bool isPrime(long long int n)
+bool checkPerfectSquare(ll_t n){
+	return (computeTotalDivisors(n) % 2 ? true : false);
+}  
+bool isPrime(ll_t n)
 {
-	if (compute_total_divisors(n)==2)
-		return true;
-	else
-		return false;
+	return (computeTotalDivisors(n)==2 ? true : false);
 } 
+//modular arithmetic function for addition
+ll_t add(ll_t a, ll_t b)
+{
+    a+=b;
+    if (a>=mod_val)
+        a-=mod_val;
+    return a;
+}
+//modular arithmetic function for multiplication
+ll_t mul(ll_t a, ll_t b)
+{
+    return ((a*b) % mod_val);
+}
+//modular arithmetic function for exponentiation
+ll_t power(ll_t base, ll_t expo)
+{
+    ll_t res=1;
+    while(expo>0) 
+    {
+       if(expo%2==1) 
+            res =(res*base) % mod_val;
+       base =(base*base) % mod_val;
+       expo /= 2;
+    }
+    return res % mod_val;
+}  
+void initializeGraph(vector<int> adj[], bool visited[]){
+	clr(adj);
+	for(int i=0; i<; i++){
+		visited[i]=0;
+	}
+}
+void printGraph(vector<int> adj[], int nodes){
+    for(int i=1; i<=nodes; i++){
+        cout << i << ": ";
+        for(int j=0; j<adj[i].size(); j++){
+            cout << adj[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+void dfs(vector<int> adj[], bool visited[], int s){
+	visited[s]=true;
+	for(int i=0; i<adj[s].size(); i++){
+		if (visited[adj[s][i]]==false)
+			dfs(adj[s][i]);
+	}
+}
 */
+
 int main()
 {
+	//code by cherrywood55
 	FAST_IO(0);
-	int t;
-	cin >> t;
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r" , stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+	int t, n;
+    cin >> t;
 	while (t--)
 	{
-		//code
+		//code by cherrywood55
+		cin >> n;
+		int a[n];
+		f(i,n)
+			cin >> a[i];
+		
+
 	}
 	return 0;
 }
